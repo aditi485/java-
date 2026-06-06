68f29e87f7ae393479a67cdec1922666d85ea46d
Aditi tripathi <tripathiaditi121@gmail.com>
2026-06-06 10:47:14 +0000

    NEW LEARNING

---
+++ lecture1/Primitive.java
@@ -0,0 +1,16 @@
+public class Primitive {
+  
+    // primitive data types are those data types which cannot be break further into nay other data types
+    public static void main(String[] args) {
+        int a = 2345; // 4 bytes
+        char b = 'b';
+        float f = 567.78f; // 4 bytes
+        double long_decimal_values = 466.798;  // 8 bytes
+        long long_integer_values = 234579842567425L;   //8 bytes
+        boolean d = true; 
+        System.out.println(a);
+    }
+    
+}
+    
+
