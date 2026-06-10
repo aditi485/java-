eb622bb29e7be797252b93c7c3084ea6ab84d374
Aditi tripathi <tripathiaditi121@gmail.com>
2026-06-10 05:29:17 +0000

    new

---
+++ assignments/lecture3/ques1.java
@@ -0,0 +1,14 @@
+package assignments.lecture3;
+// Area Of Circle Java Program
+import java.util.*;
+public class ques1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("please enter the radius : ");
+        float r = in.nextFloat();
+        float area = 3.14f*r*r;
+        System.out.println(area);
+
+    }
+    
+}
