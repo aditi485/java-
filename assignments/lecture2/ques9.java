32934bc1547cc324a442dedabe4886c28c22e941
Aditi tripathi <tripathiaditi121@gmail.com>
2026-06-10 04:18:18 +0000

    new

--- assignments/lecture2/ques9.java
+++ assignments/lecture2/ques9.java
@@ -2,7 +2,24 @@
 import java.util.*;
 public class ques9 {
     public static void main(String[] args) {
-          
+          Scanner in = new Scanner(System.in);
+          System.out.println("please enter the no.");
+          int num = in.nextInt();
+          int n1 = num;
+          int rem;
+          int sum = 0;
+          while(num>0){
+             rem = num%10;
+             sum = sum + rem*rem*rem;
+             num=num/10;
+          }
+
+          if(n1==sum){
+            System.out.println("the given no. is armstrong");
+          }
+          else{
+            System.out.println("not armstrong");
+          }
         
     }
     
