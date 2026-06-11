ede6eee2beab5511075a552848fbee55edd88d32
Aditi tripathi <tripathiaditi121@gmail.com>
2026-06-11 11:40:32 +0000

    new

---
+++ lecture4/greeting.java
@@ -0,0 +1,8 @@
+public class greeting {
+    public static void main(String[] args) {
+        greet();
+    }
+    static void greet(){
+        System.out.println("hey goodmorning, you are looking gorgeous");
+    }
+}
