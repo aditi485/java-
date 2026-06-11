2e20294dada560da26649c456d6dc44f1302020a
Aditi tripathi <tripathiaditi121@gmail.com>
2026-06-11 11:39:39 +0000

    new

---
+++ lecture4/argu_integer_example.java
@@ -0,0 +1,10 @@
+public class argu_integer_example{
+    public static void main(String[] args) {
+        int ans = sum(20,47);
+        System.out.println(ans);
+    }
+    static int sum(int a,int b){
+        int sum = a+b;
+        return sum;
+    }
+}
