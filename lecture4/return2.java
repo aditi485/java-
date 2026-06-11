8a10be349ffc6cc6586907511391856c09f69ffc
Aditi tripathi <tripathiaditi121@gmail.com>
2026-06-11 11:41:00 +0000

    new

---
+++ lecture4/return2.java
@@ -0,0 +1,9 @@
+public class return2 {
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+    static String greet(){
+        String greet = "how are you";
+        return greet;
+    }
+}
