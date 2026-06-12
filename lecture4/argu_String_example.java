f1b82cf4391fe513ce9f2b275a25cbd8cfbf1436
Aditi tripathi <tripathiaditi121@gmail.com>
2026-06-12 10:14:14 +0000

    new

--- lecture4/argu_String_example.java
+++ lecture4/argu_String_example.java
@@ -1,3 +1,11 @@
 public class argu_String_example {
-    
+    public static void main(String[] args) {
+        String message = greet("aditi tripathi");
+        System.out.println(message);
+
+    }
+    static String greet(String name){
+        String greeting = "hello"+name;
+        return greeting;
+    }
 }
