5043d1c94bb5f8bf924e8eb45c78ddf8a7d2fdcb
Aditi tripathi <tripathiaditi121@gmail.com>
2026-06-14 06:46:14 +0000

    new

---
+++ assignments/lecture4/ques7.java
@@ -0,0 +1,9 @@
+// Define a method to find out if a number is prime or not.
+package assignments.lecture4;
+import java.util.*;
+public class ques7 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
