e811c781c1814959e6fcbccf903a35d9980f20a0
Aditi tripathi <tripathiaditi121@gmail.com>
2026-06-15 06:04:43 +0000

    new

---
+++ assignments/lecture4/ques10.java
@@ -0,0 +1,22 @@
+//Write a function to find if a number is a palindrome or not. Take number as parameter.
+package assignments.lecture4;
+import java.util.*;
+public class ques10 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the number");
+        int n = in.nextInt();
+
+    }
+    static boolean palindrome(int num){
+        String rev = "";
+        while(num>0){
+            int rem = num%10;
+            
+        }
+           
+            
+
+    }
+    
+}
