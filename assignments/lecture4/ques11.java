b2099e201fd7dc81faa3fe7dbc7a09c1f7543ea9
Aditi tripathi <tripathiaditi121@gmail.com>
2026-06-16 16:09:22 +0000

    new

--- assignments/lecture4/ques11.java
+++ assignments/lecture4/ques11.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class ques11 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("please enter the no.");
         int num = in.nextInt();
         
     }
