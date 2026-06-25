62e4ba6c157669a213bd68e3c33bf48fe963f6b3
Aditi tripathi <tripathiaditi121@gmail.com>
2026-06-25 13:30:45 +0000

    c

---
+++ lecture5/max.java
@@ -0,0 +1,20 @@
+package lecture5;
+
+public class max {
+    public static void main(String[] args) {
+           int[] arr = {
+                 1,52,37,88,50,78};
+         int ans = max(arr); 
+         System.out.println(ans);
+    } 
+
+    static int max(int[] arr){
+        int max = 0 ;
+        for(int i =0;i<arr.length;i++){
+           if(arr[i]>max){
+            max = arr[i];
+           }
+        }
+    return max;
+   }
+}
