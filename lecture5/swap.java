afce61e1366a65016fe51492b9f22b294c783a51
Aditi tripathi <tripathiaditi121@gmail.com>
2026-06-25 12:17:07 +0000

    c

--- lecture5/swap.java
+++ lecture5/swap.java
@@ -1,8 +1,17 @@
 package lecture5;
 
+import java.util.Arrays;
+
 public class swap {
     public static void main(String[] args) {
-        
+        int [] arr = {1,2,3,4,5,6};
+        swap(arr,2,4);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr,int index1,int index2){
+         int temp = arr[index1];
+         arr[index1] = arr[index2];
+         arr[index2] = temp;
     }
     
 }
