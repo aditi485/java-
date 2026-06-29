12605d6e1d477cc0f26bd657df3a98ec4e20a1a7
Aditi tripathi <tripathiaditi121@gmail.com>
2026-06-29 13:08:36 +0000

    c

---
+++ lecture6/stringsearch.java
@@ -0,0 +1,25 @@
+package lecture6;
+
+public class stringsearch {
+    public static void main(String[] args) {
+        String name = "Rahul";
+        char ch ='p';
+        System.out.println(stringlinaersearch( name , ch));
+
+    }
+    static boolean stringlinaersearch(String name , char c){
+        if(name.length() == 0){
+            return false;
+
+        }
+        for(int i =0;i<name.length();i++){
+            if(c==name.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+
+
+    }
+    
+}
