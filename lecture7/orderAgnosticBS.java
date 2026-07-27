3d489c9e3bf6ff0142f7dc67b417766fd429a34b
Aditi tripathi <tripathiaditi121@gmail.com>
2026-07-27 16:02:32 +0000

    c

---
+++ lecture7/orderAgnosticBS.java
@@ -0,0 +1,9 @@
+package lecture7;
+
+public class orderAgnosticBS {
+    public static void main(String[] args) {
+        // normally we dont know that whether it is sorted in ascending or descending orders so 
+        // it is nessecary to check whether it is sorted in ascending or descending order.
+    }
+    
+}
